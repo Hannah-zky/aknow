a2b89b1dc9f874a21ba4cb40e92f37febd7fffeb
Hannah-zky <zkyharuna.tech@gmail.com>
2022-11-23 17:49:02 +0900

    各ビューファイルの調整とリンクの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :category
 
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
 
   with_options presence: true do
     validates :image, :explanation
