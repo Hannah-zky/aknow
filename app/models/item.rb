903cb930b5b28793a6b4f9acce121dbbd3dde5c3
Hannah-zky <zkyharuna.tech@gmail.com>
2022-11-23 13:59:33 +0900

    画像投稿ツールの導入とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  with_options presence: true do
+    validates :image, :explanation
+  end
 end
