7ad9446f72da44bf2e4a2ad42fbbe8f922c14006
Hannah-zky <zkyharuna.tech@gmail.com>
2022-11-23 15:57:51 +0900

    新規投稿画面のビュー作成

---
+++ app/models/category.rb
@@ -0,0 +1,16 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '選択してください' },
+    { id: 2, name: 'トップス' },
+    { id: 3, name: 'ジャケット/アウター' },
+    { id: 4, name: 'パンツ' },
+    { id: 5, name: 'スカート' },
+    { id: 6, name: 'オールインワン' },
+    { id: 7, name: 'ワンピース' },
+    { id: 8, name: 'スーツ' },
+    { id: 9, name: 'その他' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
