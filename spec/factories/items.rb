76dfa1e0cbecc4c98ca758a65a2159b62d5e1f72
Hannah-zky <zkyharuna.tech@gmail.com>
2022-11-24 17:58:14 +0900

    Itemモデル 単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    category_id       { 2 }
+    explanation       { Faker::Lorem.sentence }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/img/オムライス.png'), filename: 'オムライス.png')
+    end
   end
 end
